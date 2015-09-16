6680e8965451271a2e09c756fcc202b70aa2003f
Mebin Jacob <mebinjacob@gmail.com>
2015-09-15 22:55:12 -0400

    string to byte conversion fixed

--- src/main/scala/rdf/Query.scala
+++ src/main/scala/rdf/Query.scala
@@ -5,10 +5,11 @@ package rdf
  */
 object Query {
   def main(args: Array[String]): Unit = {
-//    val rtLoad = Runtime.getRuntime.exec(Array("rdf3xload", "rdfdb"))
-//    rtLoad.waitFor
+    //    val rtLoad = Runtime.getRuntime.exec(Array("rdf3xload", "rdfdb"))
+    //    rtLoad.waitFor
     val rtQuery = Runtime.getRuntime.exec(Array("rdf3xquery", "rdfdb"))
-    val os =rtQuery.getOutputStream
-    os.write("SELECT  ?X ?Y ?Z WHERE { ?X <http://dummy.org/sample#9> <http://dummy.org/sample#-1> . ?Y <http://dummy.org/sample#9> <http://dummy.org/sample#-7> . ?Z <http://dummy.org/sample#9> <http://dummy.org/sample#-6> . ?X <http://dummy.org/sample#6> ?Z . ?Z <http://dummy.org/sample#4> ?Y . ?X <http://dummy.org/sample#12> ?Y}".toByte)    
+    val os = rtQuery.getOutputStream
+    os.write("SELECT  ?X ?Y ?Z WHERE { ?X <http://dummy.org/sample#9> <http://dummy.org/sample#-1> . ?Y <http://dummy.org/sample#9> <http://dummy.org/sample#-7> . ?Z <http://dummy.org/sample#9> <http://dummy.org/sample#-6> . ?X <http://dummy.org/sample#6> ?Z . ?Z <http://dummy.org/sample#4> ?Y . ?X <http://dummy.org/sample#12> ?Y}".getBytes)
+    os.close
   }
 }
