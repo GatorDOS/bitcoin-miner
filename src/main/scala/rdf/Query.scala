2667928e4c264943cdc749ed6a12343a8b542c11
Mebin Jacob <mebinjacob@gmail.com>
2015-09-15 23:44:01 -0400

    query class updated

--- src/main/scala/rdf/Query.scala
+++ src/main/scala/rdf/Query.scala
@@ -7,18 +7,20 @@ import java.io.InputStreamReader
  * @author mebin
  */
 object Query {
-  var a:Array[Byte] = "1".getBytes
+  var a: Array[Byte] = "1".getBytes
   def main(args: Array[String]): Unit = {
     //    val rtLoad = Runtime.getRuntime.exec(Array("rdf3xload", "rdfdb"))
     //    rtLoad.waitFor
     val rtQueryProcess = Runtime.getRuntime.exec(Array("rdf3xquery", "rdfdb"))
     val is = rtQueryProcess.getInputStream
     val os = rtQueryProcess.getOutputStream
-    var line:String = ""
-    val in = new BufferedReader(new InputStreamReader(rtQueryProcess.getInputStream()) );
-       while ((line = in.readLine()) != null) {
-         println(line);
-       }
+
+    val in = new BufferedReader(new InputStreamReader(rtQueryProcess.getInputStream()));
+    var line: String = in.readLine()
+    while (line != null) {
+      line = in.readLine()
+      println(line);
+    }
     os.write("SELECT  ?X ?Y ?Z WHERE { ?X <http://dummy.org/sample#9> <http://dummy.org/sample#-1> . ?Y <http://dummy.org/sample#9> <http://dummy.org/sample#-7> . ?Z <http://dummy.org/sample#9> <http://dummy.org/sample#-6> . ?X <http://dummy.org/sample#6> ?Z . ?Z <http://dummy.org/sample#4> ?Y . ?X <http://dummy.org/sample#12> ?Y}".getBytes)
     is.read(a)
     println(new String(a))
