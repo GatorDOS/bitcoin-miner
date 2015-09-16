7053022349403f142af3727c6a46ef5def91bbf8
Mebin Jacob <mebinjacob@gmail.com>
2015-09-16 00:30:07 -0400

    query class updated

--- src/main/scala/rdf/Query.scala
+++ src/main/scala/rdf/Query.scala
@@ -9,16 +9,15 @@ import java.io.InputStreamReader
  * @author mebin
  */
 object Query {
-  var a: Array[Byte] = "1".getBytes
   def main(args: Array[String]): Unit = {
 
     val query = "SELECT  ?X ?Y ?Z WHERE { ?X <http://dummy.org/sample#9> <http://dummy.org/sample#-1> . ?Y <http://dummy.org/sample#9> <http://dummy.org/sample#-7> . ?Z <http://dummy.org/sample#9> <http://dummy.org/sample#-6> . ?X <http://dummy.org/sample#6> ?Z . ?Z <http://dummy.org/sample#4> ?Y . ?X <http://dummy.org/sample#12> ?Y}"
     val file = new File("q2.txt")
     val fileWriter = new FileWriter(file)
-    fileWriter.write(query)
-    fileWriter.close()
-    val rtQueryProcess = Runtime.getRuntime.exec(Array("rdf3xquery", "rdfdb", file.getAbsolutePath))
-    rtQueryProcess waitFor
+    fileWriter write query
+    fileWriter close
+    val rtQueryProcess = Runtime.getRuntime.exec(Array("rdf3xquery", "/tmp/shared/output/final/rdfdb", file.getAbsolutePath))
+//    rtQueryProcess waitFor
     val is = rtQueryProcess.getInputStream
     val in = new BufferedReader(new InputStreamReader(rtQueryProcess.getInputStream ));
     var line: String = in.readLine()
